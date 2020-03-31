69c30146bd100a2c4f0b159ce7aa010089968e77
黄光日 <463817374@qq.com>
2020-03-31 15:14:05 +0800

    解决跨域问题

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	_ "fiction_web/routers"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/plugins/cors"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -14,6 +15,20 @@ func init()  {
 }
 
 func main() {
+	//InsertFilter是提供一个过滤函数
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		//允许访问所有源
+		AllowAllOrigins: true,
+		//可选参数"GET", "POST", "PUT", "DELETE", "OPTIONS" (*为所有)
+		//其中Options跨域复杂请求预检
+		AllowMethods:   []string{"*"},
+		//指的是允许的Header的种类
+		AllowHeaders: 	[]string{"*"},
+		//公开的HTTP标头列表
+		ExposeHeaders:	[]string{"Content-Length"},
+		//如果设置，则允许共享身份验证凭据，例如cookie
+		AllowCredentials: true,
+	}))
 	beego.Run()
 }
 
